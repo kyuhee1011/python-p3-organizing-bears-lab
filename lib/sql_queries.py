b520648a839413585c2607cf4222da687e29984d
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-07-26 16:03:50 -0400

    fixed

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -16,7 +16,7 @@ select_all_bears_names_and_orders_in_alphabetical_order = """
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT name, age
     FROM bears
-    WHERE alive =TRUE
+    WHERE alive
     ORDER BY bears.age;
 """
 
